f8daed8e61b4cb60e3bbccfff1d034b7dff77935
jatin Bhagat <jatinbhagat7766@gmail.com>
2024-10-18 15:22:24 -0600

    test

--- test.py
+++ test.py
@@ -1,41 +1,34 @@
-def oddeoreven(x):
-    z=x%2
-    if z==0:
-        print("even")
-    else: 
-        print("odd")
+# # x=5
 
-#oddeoreven(3)#
-p = "pickle"
+# # def someFunc(g):
+# #     global x
+# #     x=10
+# #     return
 
-#print(p[1])
-#print(p[-1])
-#print(p[0:3])
-#print("w" in p)
-#print(len(p))
-my = "my # is "
-a = 5
-#print(my,a)
-mylist = ["apple", "banana", "cherry",["yoo","hi"],20]
-#print(mylist[2][2])
+# # print(x)
+# # someFunc(x)
 
-#quiz 2 
-#class Door:
-#    colour = 'yellow'
-
-#    def __init__(self, material, status):
-#        self.material = material 
-#        self.status = status
-#
-#    def open(self):
-#        self.status = 'open'
-#
-#    def close(self):
-#        self.status = 'closed'
-
-#food = [['pizza', 'kraft dinner'], ['hot dogs', 'pancakes', 'cereal', 'toast'], 'steak']
-
-#################################
+# # print(x)
 
 
+# list1 = ['a', 'b', 'd', 'y', 'v', 'w', 'x', 't', 'x', 'q']
+# def fl(c, v):
+#     for x in range(len(v)):
+#         if v[x] == c:
+#             print(x)
+#             return
+#     print(False)
+#     return
+
+# fl('x',list1)
+
+# print(len(list1))
+
+# for x in range(len(list1)):
+#     print(x)
+
+x = 7 
+for i in range(10,2):
+    print("oh my", end = " ")
+print("here")
 
