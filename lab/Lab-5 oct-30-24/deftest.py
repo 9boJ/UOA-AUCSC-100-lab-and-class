2c36454fb169e59fe12a774be1f3d26cd2d881dc
jatin Bhagat <jatinbhagat7766@gmail.com>
2024-11-01 09:48:51 -0600

    leb test

---
+++ lab/Lab-5 oct-30-24/deftest.py
@@ -0,0 +1,37 @@
+# def avg(l1):
+#     for i in range(0,len(l1)):
+#         for x in range(0,len(l1[i])):
+#             make = 0
+#             for z in range(0,len(l1[i][x])):
+#                 make = make + l1[i][x][z]
+#                 print(make)
+
+
+# olist = [[95,92,87],[66,54],[89,72,100],[33,0,0]]
+# avg(olist)
+
+# print(len(olist))
+
+
+
+
+
+# def hit(x1,y1,x2,y2,r):
+#     ans = False
+#     xc = x1 + r
+#     if  x2 <= r:
+#         ans = True
+#     elif >
+#         ans = False
+    
+
+# atom = ["A","B","C","D","E","F","G","H","I","J","K","L","M"]
+
+# def partition(l1):
+#     for i in range(len(l1)):
+#         for z in range(len(atom)):
+#             if l1[i][0] == atom[z]:
+#                 print(l1[i])
+
+# partition(["Eleanor","Evelyn","Sammy","Owen","Gavin"])
+
